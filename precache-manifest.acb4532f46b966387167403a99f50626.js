997d71782b07d4ef0fa0e0c021e25ba7ca6918d9
Abubakar Ibrahim <abubakaribrahim08@yahoo.com>
2019-03-05 00:38:52 +0300

    Updates

---
+++ precache-manifest.acb4532f46b966387167403a99f50626.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d01f72fd3be3c564f56b",
+    "url": "/bookfindr/static/js/runtime~main.d01f72fd.js"
+  },
+  {
+    "revision": "bf2cfa5567d13d8538d9",
+    "url": "/bookfindr/static/js/main.bf2cfa55.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/bookfindr/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "bf2cfa5567d13d8538d9",
+    "url": "/bookfindr/static/css/main.b4045e39.chunk.css"
+  },
+  {
+    "revision": "44e64a7cfe3c03960ece2df4537f2a72",
+    "url": "/bookfindr/index.html"
+  }
+];
